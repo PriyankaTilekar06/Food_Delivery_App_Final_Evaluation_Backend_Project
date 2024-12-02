125496ae00efbf839bd76d6c9cfae06fd780c002
Priyanka <priyankatilekar003@gmail.com>
2024-12-02 18:21:52 +0530

    committed

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,14 +1,14 @@
 const express = require("express")
 const router = express.Router()
-const cors = require('cors')
+// const cors = require('cors')
 const { registerUser, loginUser, getProfile } = require('../controllers/authControllers')
 
-router.use(
-    cors({
-        credentials: true,
-        origin: 'https://food-delivery-app-final-evaluation-frontend-project-2oxo.vercel.app/'
-    })
-)
+// router.use(
+//     cors({
+//         credentials: true,
+//         origin: 'https://food-delivery-app-final-evaluation-frontend-project-2oxo.vercel.app'
+//     })
+// )
 
 router.post('/register', registerUser)
 router.post('/login', loginUser)
